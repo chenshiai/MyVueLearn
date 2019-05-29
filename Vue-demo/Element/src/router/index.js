f56365b3ddd5a723bac8427860c394a8c3da314b
chenshuai <1183942503@qq.com>
2019-05-29 21:25:34 +0800

    重新配置了一下

--- Vue-demo/Element/src/router/index.js
+++ Vue-demo/Element/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
+import Main from '@/components/Main'
+import MainDetail from '@/components/MainDetail'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +17,18 @@ export default new Router({
       path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/main',
+      name: Main,
+      component: Main,
+      children: [
+        {
+          path: '/',
+          name: MainDetail,
+          component: MainDetail
+        }
+      ]
     }
   ]
 })
